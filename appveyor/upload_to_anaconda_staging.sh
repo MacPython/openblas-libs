b554622f91b801a21d1d7e3d3619c2ec90c86b48
Olivier Grisel <olivier.grisel@ensta.org>
2020-02-17 16:19:57 +0100

    Skip upload is token is missing

--- appveyor/upload_to_anaconda_staging.sh
+++ appveyor/upload_to_anaconda_staging.sh
@@ -9,12 +9,16 @@ pushd OpenBLAS
 VERSION=$(git describe --tags)
 popd
 
-echo "Uploading OpenBLAS $VERSION to anaconda.org staging:"
-ls -lh builds/openblas*.zip
+if [ "$OPENBLAS_LIBS_STAGING_UPLOAD_TOKEN" == "" ]; then
+    echo "OPENBLAS_LIBS_STAGING_UPLOAD_TOKEN is not defined: skipping."
+else
+    echo "Uploading OpenBLAS $VERSION to anaconda.org staging:"
+    ls -lh builds/openblas*.zip
 
-anaconda -t $OPENBLAS_LIBS_STAGING_UPLOAD_TOKEN upload \
-          --no-progress --force -u multibuild-wheels-staging \
-          -t file -p "openblas-libs" -v "$VERSION" \
-          -d "OpenBLAS for multibuild wheels" \
-          -s "OpenBLAS for multibuild wheels" \
-          builds/openblas*.zip
+    anaconda -t $OPENBLAS_LIBS_STAGING_UPLOAD_TOKEN upload \
+            --no-progress --force -u multibuild-wheels-staging \
+            -t file -p "openblas-libs" -v "$VERSION" \
+            -d "OpenBLAS for multibuild wheels" \
+            -s "OpenBLAS for multibuild wheels" \
+            builds/openblas*.zip
+fi
