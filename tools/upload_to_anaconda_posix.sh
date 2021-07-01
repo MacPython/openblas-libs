840924e7cf21bbdaeb4f5df03c29d00d921b9894
mattip <matti.picus@gmail.com>
2021-07-01 10:45:46 +0300

    try to open permissions on /home/travis/.cache

--- tools/upload_to_anaconda_posix.sh
+++ tools/upload_to_anaconda_posix.sh
@@ -4,6 +4,7 @@ set -ex
 # Note that the anaconda-client package on PyPI is too old. Install from github
 # tag instead:
 echo $(python -c "import sys; print(sys.version)")
+sudo chmod -R a+w /home/travis/.cache
 pip install -q git+https://github.com/Anaconda-Platform/anaconda-client@1.8.0
 upload_to_anaconda
 
