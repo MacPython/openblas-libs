af615aa30bbdc7fb0f4a26a7ff9ed236954eeaf4
Matthew Brett <matthew.brett@gmail.com>
2022-06-20 17:02:27 +0100

    Refactor main build script
    Allow use of some command line arguments.
    
    Make defaults for everything.
    
    Pass LDFLAGS

--- tools/build_openblas.sh
+++ tools/build_openblas.sh
@@ -1,40 +1,62 @@
 #!/bin/bash
 # Build script for OpenBLAS on Windows
-# Expects environment variables:
-#  OPENBLAS_ROOT
-#  OPENBLAS_COMMIT
-#  BUILD_BITS
-#  START_DIR
-# Expects "lib.exe" and "gcc" to be on the path
+#
+# Usage: build_openblas.sh [openblas_root [build_bits [if_bits]]]
+#
+# e.g build_openblas.sh c:\\opt 64 32
+#
+# Uses the optional environment variables.  We always prefer command line argument
+# values above to environment variable values:
+#
+#  OPENBLAS_ROOT  (default directory root for binaries, unspecified -> c:\opt).
+#  BUILD_BITS  (default binary architecture, 32 or 64, unspec -> 64).
+#  INTERFACE64  (1 for 64-bit interface, anything else or undefined for 32,
+#                This gives the default value if if_bits not specified above).
+#  START_DIR  (directory containing OpenBLAS source, unspec -> .. from here)
+#  OPENBLAS_COMMIT  (unspec -> current submodule commit)
+#  LDFLAGS  (example: "-lucrt -static -static-libgcc")
+#
+# Expects at leasts these binaries on the PATH:
+# realpath, cygpath, zip, gcc, make, ar, dlltool
+# usually as part of an msys installation.
 
-set -ex
-
-# Paths in Unix format
-OPENBLAS_ROOT=$(cygpath "$OPENBLAS_ROOT")
-
+# Convert to Unix-style path
+openblas_root="$(cygpath ${1:-${OPENBLAS_ROOT:-c:\\opt}})"
+build_bits="${2:-${BUILD_BITS:-64}}"
+if [ "$INTERFACE64" == "1" ]; then if_default=64; else if_default=32; fi
+if_bits=${3:-${if_default}}
 # Our directory for later copying
-our_wd=$(cygpath "$START_DIR")
-cd $our_wd
-# Make output directory for build artifacts
-rm -rf builds
-mkdir builds
+if [ -z "$START_DIR" ]; then
+    our_wd="$(realpath $(dirname "${BASH_SOURCE[0]}")/..)"
+else
+    our_wd=$(cygpath "$START_DIR")
+fi
 
-cd OpenBLAS
+echo "Building from $our_wd, to $openblas_root"
+echo "Binaries are $build_bits bit, interface is $if_bits bit"
+echo "Using gcc at $(which gcc), --version:"
+gcc --version
+
+# Make output directory for build artifacts
+builds_dir="$our_wd/builds"
+rm -rf $builds_dir
+mkdir $builds_dir
+
+cd "${our_wd}/OpenBLAS"
 git submodule update --init --recursive
 
-# Check which gcc we're using
-which gcc
-gcc --version
 
 # Get / clean code
 git fetch origin
-git checkout $OPENBLAS_COMMIT
+if [ -n "$OPENBLAS_COMMIT" ]; then
+    git checkout $OPENBLAS_COMMIT
+fi
 git clean -fxd
 git reset --hard
-rm -rf $OPENBLAS_ROOT/$BUILD_BITS
+rm -rf $openblas_root/$build_bits
 
 # Set architecture flags
-if [ "$BUILD_BITS" == 64 ]; then
+if [ "$build_bits" == 64 ]; then
     march="x86-64"
     # https://csharp.wekeepcoding.com/article/10463345/invalid+register+for+.seh_savexmm+in+Cygwin
     extra="-fno-asynchronous-unwind-tables"
@@ -51,9 +73,9 @@ cflags="-O2 -march=$march -mtune=generic $extra"
 fflags="$fextra $cflags -frecursive -ffpe-summary=invalid,zero"
 
 # Set suffixed-ILP64 flags
-if [ "$INTERFACE64" == "1" ]; then
-    interface64_flags="INTERFACE64=1 SYMBOLSUFFIX=64_"
-    SYMBOLSUFFIX=64_
+if [ "$if_bits" == "64" ]; then
+    SYMBOLSUFFIX="64_"
+    interface64_flags="INTERFACE64=1 SYMBOLSUFFIX=${SYMBOLSUFFIX}"
     # We override FCOMMON_OPT, so we need to set default integer manually
     fflags="$fflags -fdefault-integer-8"
 else
@@ -66,42 +88,50 @@ OPENBLAS_VERSION=$(git describe --tags)
 # Build OpenBLAS
 # Variable used in creating output libraries
 export LIBNAMESUFFIX=${OPENBLAS_VERSION}-${GCC_TAG}
-make BINARY=$BUILD_BITS DYNAMIC_ARCH=1 USE_THREAD=1 USE_OPENMP=0 \
+make BINARY=$build_bits DYNAMIC_ARCH=1 USE_THREAD=1 USE_OPENMP=0 \
      NUM_THREADS=24 NO_WARMUP=1 NO_AFFINITY=1 CONSISTENT_FPCSR=1 \
      BUILD_LAPACK_DEPRECATED=1 TARGET=PRESCOTT BUFFERSIZE=20\
+     LDFLAGS="$LDFLAGS" \
      COMMON_OPT="$cflags" \
      FCOMMON_OPT="$fflags" \
      MAX_STACK_ALLOC=2048 \
      $interface64_flags
-make PREFIX=$OPENBLAS_ROOT/$BUILD_BITS $interface64_flags install
+make PREFIX=$openblas_root/$build_bits $interface64_flags install
 DLL_BASENAME=libopenblas${SYMBOLSUFFIX}_${LIBNAMESUFFIX}
-if [ "$INTERFACE64" == "1" ]; then
+if [ "$if_bits" == "64" ]; then
     # OpenBLAS does not build a symbol-suffixed static library on Windows:
     # do it ourselves
     set -x  # echo commands
     static_libname=$(find . -maxdepth 1 -type f -name '*.a' \! -name '*.dll.a' | tail -1)
     make -C exports $interface64_flags objcopy.def
     objcopy --redefine-syms exports/objcopy.def "${static_libname}" "${static_libname}.renamed"
-    cp -f "${static_libname}.renamed" "$OPENBLAS_ROOT/$BUILD_BITS/lib/${static_libname}"
-    cp -f "${static_libname}.renamed" "$OPENBLAS_ROOT/$BUILD_BITS/lib/${DLL_BASENAME}.a"
+    cp -f "${static_libname}.renamed" "$openblas_root/$build_bits/lib/${static_libname}"
+    cp -f "${static_libname}.renamed" "$openblas_root/$build_bits/lib/${DLL_BASENAME}.a"
     set +x
 fi
-cd $OPENBLAS_ROOT
+cd $openblas_root
 # Copy library link file for custom name
-cd $BUILD_BITS/lib
+cd $build_bits/lib
+cp ${our_wd}/OpenBLAS/exports/${DLL_BASENAME}.def ${DLL_BASENAME}.def
 # At least for the mingwpy wheel, we have to use the VC tools to build the
 # export library. Maybe fixed in later binutils by patch referred to in
 # https://sourceware.org/ml/binutils/2016-02/msg00002.html
-cp ${our_wd}/OpenBLAS/exports/${DLL_BASENAME}.def ${DLL_BASENAME}.def
-"lib.exe" /machine:${vc_arch} /def:${DLL_BASENAME}.def
+# "lib.exe" /machine:${vc_arch} /def:${DLL_BASENAME}.def
+# Maybe this will now work (after 2016 patch above).
+dlltool --input-def ${DLL_BASENAME}.def \
+    --output-exp ${DLL_BASENAME}.exp \
+    --dllname ${DLL_BASENAME}.dll \
+    --output-lib ${DLL_BASENAME}.lib
+# Replace the DLL name with the generated name.
+sed -i "s/ -lopenblas$/ -l${DLL_BASENAME:3}/g" pkgconfig/openblas.pc
 cd ../..
 # Build template site.cfg for using this build
-cat > ${BUILD_BITS}/site.cfg.template << EOF
+cat > ${build_bits}/site.cfg.template << EOF
 [openblas${SYMBOLSUFFIX}]
 libraries = $DLL_BASENAME
-library_dirs = {openblas_root}\\${BUILD_BITS}\\lib
-include_dirs = {openblas_root}\\${BUILD_BITS}\\include
+library_dirs = {openblas_root}\\${build_bits}\\lib
+include_dirs = {openblas_root}\\${build_bits}\\include
 EOF
-ZIP_NAME="openblas${SYMBOLSUFFIX}-${OPENBLAS_VERSION}-${plat_tag}-${GCC_TAG}.zip"
-zip -r $ZIP_NAME $BUILD_BITS
-cp $ZIP_NAME $our_wd/builds
+zip_name="openblas${SYMBOLSUFFIX}-${OPENBLAS_VERSION}-${plat_tag}-${GCC_TAG}.zip"
+zip -r $zip_name $build_bits
+cp $zip_name ${builds_dir}
