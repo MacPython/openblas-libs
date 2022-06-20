df2b6938bfbaddb5255c81a02569ac0983e65b95
Matthew Brett <matthew.brett@gmail.com>
2022-06-20 17:03:36 +0100

    Refactor build to use Powershell
    Refactor build_gfortran to run builds in own directory.

--- tools/build_gfortran.sh
+++ tools/build_gfortran.sh
@@ -2,16 +2,23 @@
 
 set -e
 
-cd $(cygpath "$START_DIR")
+rm -rf for_test
+mkdir for_test
+cd for_test
+
+repo_path=$(cygpath "$START_DIR")
 OBP=$(cygpath $OPENBLAS_ROOT\\$BUILD_BITS)
 
 static_libname=$(find $OBP/lib -maxdepth 1 -type f -name '*.a' \! -name '*.dll.a' | tail -1)
 dynamic_libname=$(find $OBP/lib -maxdepth 1 -type f -name '*.dll.a' | tail -1)
+dll_name=$(echo $dynamic_libname | sed 's#/lib/#/bin/#' | sed 's/.a$//')
+
+cp $dll_name .
 
 if [ "$INTERFACE64" == "1" ]; then
-  gfortran -I $OBP/include -fdefault-integer-8 -o test.exe test64_.f90 $static_libname
-  gfortran -I $OBP/include -fdefault-integer-8 -o test_dyn.exe test64_.f90 $dynamic_libname
+  gfortran -I $OBP/include -fdefault-integer-8 -o test.exe ${repo_path}/test64_.f90 $static_libname
+  gfortran -I $OBP/include -fdefault-integer-8 -o test_dyn.exe ${repo_path}/test64_.f90 $dynamic_libname
 else
-  gfortran -I $OBP/include -o test.exe test.f90 $static_libname
-  gfortran -I $OBP/include -o test_dyn.exe test.f90 $dynamic_libname
+  gfortran -I $OBP/include -o test.exe ${repo_path}/test.f90 $static_libname
+  gfortran -I $OBP/include -o test_dyn.exe ${repo_path}/test.f90 $dynamic_libname
 fi
