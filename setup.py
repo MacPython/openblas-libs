b840e12d3026a17e83446a9095f5fb76455c7851
mayeut <mayeut@users.noreply.github.com>
2022-10-23 18:08:02 +0200

    create an ABI3 wheel when possible

--- setup.py
+++ setup.py
@@ -1,23 +1,41 @@
 import os
+import sys
 from setuptools import setup, Extension
+from wheel.bdist_wheel import bdist_wheel
+
 
 mydir = os.path.abspath(os.path.dirname(__file__))
+
+
+class bdist_wheel_abi3(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = bdist_wheel.get_tag(self)
+        return python, "abi3", plat
+
 
 # TODO: determine if we are building 64- or 32- bit interfaces
 use_64=True
 if use_64:
-    libraries = ["openblas64_",]
     macros = [("SUFFIX", "64_")]
 else:
-    libraries = ["openblas",]
     macros = []
 
+if sys.implementation.name == "cpython":
+    cmdclass = {"bdist_wheel": bdist_wheel_abi3}
+    py_limited_api = {"py_limited_api": True}
+    macros.append(('Py_LIMITED_API', '0x03070000'))
+else:
+    cmdclass = {}
+    py_limited_api = {}
+
 setup(
+    cmdclass=cmdclass,
     ext_modules=[Extension(
-            "openblas._init_openblas", ["src/_init_openblas.c"],
-            libraries=libraries,
-            library_dirs=[os.path.join(mydir, 'local', 'openblas', 'lib'),],
-            extra_link_args=["-Wl,-rpath,$ORIGIN/lib"],
-            define_macros=macros,
+        "openblas._init_openblas", ["src/_init_openblas.c"],
+        libraries=["openblas_python"],
+        library_dirs=[os.path.join(mydir, 'local', 'openblas', 'lib'),],
+        extra_link_args=["-Wl,-rpath,$ORIGIN/lib"],
+        define_macros=macros,
+        **py_limited_api
     )],
 )
