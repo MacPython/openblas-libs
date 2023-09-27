de1f703c980bbfae2f2873f2f8b00bdd3f788c64
mattip <matti.picus@gmail.com>
2023-09-27 11:39:57 +0300

    fix uploading

--- tools/upload_to_anaconda_staging.sh
+++ tools/upload_to_anaconda_staging.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 # Upload tar.gz and wheels to ananconda.org
 
-set -e
+upload_wheels() {
+    set -e
+    if [[ "$(uname -s)" == CYGWIN* ]]; then
+        our_wd=$(cygpath "$START_DIR")
+        cd $our_wd
+    fi
 
-if [[ "$(uname -s)" == CYGWIN* ]]; then 
-    our_wd=$(cygpath "$START_DIR")
-    cd $our_wd
-fi
+    pushd OpenBLAS
+    VERSION=$(git describe --tags --abbrev=8)
+    popd
 
-pushd OpenBLAS
-VERSION=$(git describe --tags --abbrev=8)
-popd
+    if [ "$ANACONDA_SCIENTIFIC_PYTHON_UPLOAD" == "" ]; then
+        echo "ANACONDA_SCIENTIFIC_PYTHON_UPLOAD is not defined: skipping."
+    else
+        echo "Uploading OpenBLAS $VERSION to anaconda.org staging:"
 
-if [ "$ANACONDA_SCIENTIFIC_PYTHON_UPLOAD" == "" ]; then
-    echo "ANACONDA_SCIENTIFIC_PYTHON_UPLOAD is not defined: skipping."
-else
-    echo "Uploading OpenBLAS $VERSION to anaconda.org staging:"
+        anaconda -t $ANACONDA_SCIENTIFIC_PYTHON_UPLOAD upload \
+                --no-progress --force -u scientific-python-nightly-wheels \
+                -t file -p "openblas-libs" -v "$VERSION" \
+                -d "OpenBLAS for multibuild wheels" \
+                -s "OpenBLAS for multibuild wheels" \
+                builds/openblas*.zip libs/openblas*.tar.gz
 
-    anaconda -t $ANACONDA_SCIENTIFIC_PYTHON_UPLOAD upload \
-            --no-progress --force -u scientific-python-nightly-wheels \
-            -t file -p "openblas-libs" -v "$VERSION" \
-            -d "OpenBLAS for multibuild wheels" \
-            -s "OpenBLAS for multibuild wheels" \
-            builds/openblas*.zip libs/openblas*.tar.gz
-
-    anaconda -t $ANACONDA_SCIENTIFIC_PYTHON_UPLOAD upload \
-            --no-progress --force -u scientific-python-nightly-wheels \
-            dist/scipy_openblas*.whl
-fi
+        anaconda -t $ANACONDA_SCIENTIFIC_PYTHON_UPLOAD upload \
+                --no-progress --force -u scientific-python-nightly-wheels \
+                dist/scipy_openblas*.whl
+    fi
+}
