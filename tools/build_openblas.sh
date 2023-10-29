d2c52da42ca297b74db61a7b6e9dae9ac022bd0b
mattip <matti.picus@gmail.com>
2023-10-29 17:12:46 +0200

    use quotes on windows, fix lib name on posix

--- tools/build_openblas.sh
+++ tools/build_openblas.sh
@@ -95,17 +95,17 @@ make BINARY=$build_bits DYNAMIC_ARCH=1 USE_THREAD=1 USE_OPENMP=0 \
      COMMON_OPT="$cflags" \
      FCOMMON_OPT="$fflags" \
      MAX_STACK_ALLOC=2048 \
-     SYMBOLPREFIX=scipy_ \
+     SYMBOLPREFIX="scipy_" \
      $interface64_flags
 make PREFIX=$openblas_root/$build_bits $interface64_flags \
- .   SYMBOLPREFIX=scipy_ install
+ .   SYMBOLPREFIX="scipy_" install
 DLL_BASENAME=libopenblas${SYMBOLSUFFIX}_${LIBNAMESUFFIX}
 if [ "$if_bits" == "64" ]; then
     # OpenBLAS does not build a symbol-suffixed static library on Windows:
     # do it ourselves
     set -x  # echo commands
     static_libname=$(find . -maxdepth 1 -type f -name '*.a' \! -name '*.dll.a' | tail -1)
-    make -C exports SYMBOLPREFIX=scipy_ $interface64_flags objcopy.def
+    make -C exports SYMBOLPREFIX="scipy_" $interface64_flags objcopy.def
     objcopy --redefine-syms exports/objcopy.def "${static_libname}" "${static_libname}.renamed"
     cp -f "${static_libname}.renamed" "$openblas_root/$build_bits/lib/${static_libname}"
     cp -f "${static_libname}.renamed" "$openblas_root/$build_bits/lib/${DLL_BASENAME}.a"
