9b0a3b3e45d2e14dcc7c40a73fc908b9c68af1c7
mattip <matti.picus@gmail.com>
2024-07-27 22:08:15 +0300

    debug

--- tools/build_steps.sh
+++ tools/build_steps.sh
@@ -177,10 +177,10 @@ function do_build_lib {
     CFLAGS="$CFLAGS -fvisibility=protected -Wno-uninitialized" \
     make BUFFERSIZE=20 DYNAMIC_ARCH=1 \
         USE_OPENMP=0 NUM_THREADS=64 \
-        BINARY=$bitness $interface_flags $target_flags shared > /dev/null
+        BINARY=$bitness $interface_flags $target_flags shared 
     make BUFFERSIZE=20 DYNAMIC_ARCH=1 \
         USE_OPENMP=0 NUM_THREADS=64 \
-        BINARY=$bitness $interface_flags $target_flags tests
+        BINARY=$bitness $interface_flags $target_flags tests > /dev/null
     make PREFIX=$BUILD_PREFIX $interface_flags install
     popd
     stop_spinner
