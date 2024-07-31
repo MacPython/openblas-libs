5c98be1590815014444ac62e6c80eaab6c665525
Matti Picus <matti.picus@gmail.com>
2024-07-31 22:17:41 +0300

    add windows_build.bat to build locally

---
+++ tools/windows_build.bat
@@ -0,0 +1,13 @@
+rem Build x86_64 OpenBLAS locally with 64-bit interfaces
+rem Requires c:\rtools40
+set BASH_PATH=c:\rtools40\usr\bin\bash.exe
+set BUILD_BITS=64
+set CHERE_INVOKING=yes
+set INTERFACE64=1
+set LDFLAGS=-lucrt -static -static-libgcc -Wl,--defsym,quadmath_snprintf=snprintf
+set MSYSTEM=UCRT64
+set PLAT=x86_64
+set START_DIR=d:\pypy_stuff\openblas-libs
+rmdir /q /s c:\opt\64
+rmdir /q /s c:\opt\64
+rem %BASH_PATH% -lc tools/build_openblas.sh
