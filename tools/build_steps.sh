a92609cc55b17008188ad07c83e57e7084494a77
mattip <matti.picus@gmail.com>
2025-05-25 08:57:27 +0300

    add debugging to clean_code step

--- tools/build_steps.sh
+++ tools/build_steps.sh
@@ -24,6 +24,28 @@ function before_build {
     fi
 }
 
+function clean_code_local {
+    set -ex
+    # Copied from common_utils.sh, with added debugging
+    local repo_dir=${1:-$REPO_DIR}
+    local build_commit=${2:-$BUILD_COMMIT}
+    [ -z "$repo_dir" ] && echo "repo_dir not defined" && exit 1
+    [ -z "$build_commit" ] && echo "build_commit not defined" && exit 1
+    # The package $repo_dir may be a submodule. git submodules do not
+    # have a .git directory. If $repo_dir is copied around, tools like
+    # Versioneer which require that it be a git repository are unable
+    # to determine the version.  Give submodule proper git directory
+    # XXX no need to do this
+    # fill_submodule "$repo_dir"
+    pushd $repo_dir
+    git fetch origin --tags
+    git checkout $build_commit
+    git clean -fxd 
+    git reset --hard
+    git submodule update --init --recursive
+    popd
+}
+
 function get_plat_tag {
     # Copied from gfortran-install/gfortran_utils.sh, modified for MB_ML_LIBC
 
--- tools/local_build.sh
+++ tools/local_build.sh
@@ -37,6 +37,27 @@ function install_virtualenv {
     pip install virtualenv
 }
 
+function clean_code_local {
+    # Copied from common_utils.sh, with added debugging
+    local repo_dir=${1:-$REPO_DIR}
+    local build_commit=${2:-$BUILD_COMMIT}
+    [ -z "$repo_dir" ] && echo "repo_dir not defined" && exit 1
+    [ -z "$build_commit" ] && echo "build_commit not defined" && exit 1
+    # The package $repo_dir may be a submodule. git submodules do not
+    # have a .git directory. If $repo_dir is copied around, tools like
+    # Versioneer which require that it be a git repository are unable
+    # to determine the version.  Give submodule proper git directory
+    # XXX no need to do this
+    # fill_submodule "$repo_dir"
+    pushd $repo_dir
+    git fetch origin --tags
+    git checkout $build_commit
+    git clean -fxd 
+    git reset --hard
+    git submodule update --init --recursive
+    popd
+}
+
 function build_openblas {
     if [[ -z VIRTUAL_ENV ]]; then
         echo "must be run in a virtualenv"
