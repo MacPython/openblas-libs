b836842a00568698e0af800e08c7d1265dc81e23
mattip <matti.picus@gmail.com>
2025-10-31 08:31:52 +0200

    fixes for windows and win-arm64

--- tools/build_steps_win_arm64.bat
+++ tools/build_steps_win_arm64.bat
@@ -97,7 +97,7 @@ for /f "usebackq tokens=*" %%i in (`"C:\Program Files (x86)\Microsoft Visual Stu
 PATH=C:\Program Files\LLVM\bin;%PATH%
 
 :: Run CMake and Ninja build
-set CFLAGS="-Wno-reserved-macro-identifier -Wno-unsafe-buffer-usage"
+set CFLAGS=-Wno-reserved-macro-identifier -Wno-unsafe-buffer-usage
 cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DUSE_THREADS=1 -DNUM_THREADS=24 -DTARGET=ARMV8 -DBUILD_SHARED_LIBS=ON -DARCH=arm64 ^
 -DBINARY=%build_bits% -DCMAKE_SYSTEM_PROCESSOR=ARM64 -DCMAKE_C_COMPILER=clang-cl ^
 -DCMAKE_Fortran_COMPILER=flang-new -DSYMBOLPREFIX="scipy_" -DLIBNAMEPREFIX="scipy_" %interface_flags%
