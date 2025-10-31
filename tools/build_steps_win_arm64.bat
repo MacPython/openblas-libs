d869e1080df9c86673a93962830a1d598856709f
mattip <matti.picus@gmail.com>
2025-10-31 10:45:16 +0200

    make win-arm64 build more like upstream

--- tools/build_steps_win_arm64.bat
+++ tools/build_steps_win_arm64.bat
@@ -91,19 +91,32 @@ mkdir build || exit /b 1 & cd build || exit /b 1
 echo Setting up ARM64 Developer Command Prompt and running CMake...
  
 :: Initialize VS ARM64 environment
-for /f "usebackq tokens=*" %%i in (`"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath`) do call "%%i\VC\Auxiliary\Build\vcvarsall.bat" arm64
+CALL "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsarm64.bat"
  
 :: Prefer LLVM flang
 PATH=C:\Program Files\LLVM\bin;%PATH%
 
 :: Run CMake and Ninja build
+
 set CFLAGS=-Wno-reserved-macro-identifier -Wno-unsafe-buffer-usage
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DUSE_THREADS=1 -DNUM_THREADS=24 -DTARGET=ARMV8 -DBUILD_SHARED_LIBS=ON -DARCH=arm64 ^
--DBINARY=%build_bits% -DCMAKE_SYSTEM_PROCESSOR=ARM64 -DCMAKE_C_COMPILER=clang-cl ^
--DCMAKE_Fortran_COMPILER=flang-new -DSYMBOLPREFIX="scipy_" -DLIBNAMEPREFIX="scipy_" %interface_flags%
+cmake .. -G Ninja ^
+ -DCMAKE_BUILD_TYPE=Release ^
+ -DTARGET=ARMV8 ^
+ -DBINARY=%build_bits% ^
+ -DCMAKE_C_COMPILER=clang-cl ^
+ -DCMAKE_Fortran_COMPILER=flang-new ^
+ -DBUILD_SHARED_LIBS=ON ^
+ -DCMAKE_SYSTEM_PROCESSOR=arm64 ^
+ -DCMAKE_SYSTEM_NAME=Windows ^
+ -DSYMBOLPREFIX="scipy_" ^
+ -DLIBNAMEPREFIX="scipy_" ^
+ -DUSE_THREADS=1 ^
+ -DNUM_THREADS=24 ^
+ %interface_flags%
+
 if errorlevel 1 exit /b 1
 
-ninja
+ninja -j 16
 if errorlevel 1 exit /b 1
  
 echo Build complete. Returning to Batch.
